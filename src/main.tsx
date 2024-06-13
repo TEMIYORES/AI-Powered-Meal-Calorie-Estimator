9ab1de28e4dcf1864f6c427ba841ed54ed206481
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-13 23:07:14 +0100

    added studyplan page

--- src/main.tsx
+++ src/main.tsx
@@ -13,12 +13,16 @@ import { store } from "./app/store.tsx";
 import { Toaster } from "sonner";
 import Register from "./features/auth/Register.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
+import StudyPlan from "./pages/StudyPlan.tsx";
+import LogSession from "./pages/LogSession.tsx";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       {/*start- Public routes */}
       <Route index={true} element={<Login />} />
       <Route path={"/dashboard"} element={<Dashboard />} />
+      <Route path={"/studyplan"} element={<StudyPlan />} />
+      <Route path={"/logsession"} element={<LogSession />} />
       <Route path="register" element={<Register />} />
       <Route path={"*"} element={<Login />} />
     </Route>
---
+++ src/pages/StudyPlan.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const StudyPlan = () => {
+  return (
+    <div className="flex h-[90vh] text-center justify-center items-center text-textcolor text-2xl">
+      <Header />
+      StudyPlan
+    </div>
+  );
+};
+
+export default StudyPlan;
