e0563ef2287c04b67a73750e72e43ce9e1eb0a89
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-13 23:01:53 +0100

    started header component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,15 @@
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
 import { getUserLoggedIn } from "../features/store/auth/authSlice";
+import Header from "../components/Header";
 
 const Dashboard = () => {
   const userLoggedIn = useSelector(getUserLoggedIn);
   console.log({ userLoggedIn });
   return (
     <>
-      {!userLoggedIn && <Navigate to={"/login"} replace={true} />}
-      <h1 className="text-textcolor h-[90vh] text-2xl flex justify-center items-center">
-        Welcome to Dashboard!
-      </h1>
-      ;
+      <main className="relative w-full min-h-screen bg-bg flex flex-col self-center px-[2%] py-3 transition-all duration-500 ease-in-out md:px-[5%] lg:px-[10%]">
+        <Header />
+      </main>
     </>
   );
 };
