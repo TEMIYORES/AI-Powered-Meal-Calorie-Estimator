75c83d331397349af38771654ea18e9ffb2a68fe
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-13 23:07:33 +0100

    added logsession page

---
+++ src/pages/LogSession.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const LogSession = () => {
+  return (
+    <div className="flex h-[90vh] text-center items-center justify-center text-textcolor text-2xl">
+      <Header />
+      Log sessions
+    </div>
+  );
+};
+
+export default LogSession;
