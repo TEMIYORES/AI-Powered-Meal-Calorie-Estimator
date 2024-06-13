1c1ea806fbedac0db81cf14008d478692b28ad07
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-13 23:12:42 +0100

    updated dashboard content and styles

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,6 +9,9 @@ const Dashboard = () => {
     <>
       <main className="relative w-full min-h-screen bg-bg flex flex-col self-center px-[2%] py-3 transition-all duration-500 ease-in-out md:px-[5%] lg:px-[10%]">
         <Header />
+        <div className="text-center text-2xl flex justify-center items-center text-textcolor h-[70vh]">
+          Dashboard
+        </div>
       </main>
     </>
   );
