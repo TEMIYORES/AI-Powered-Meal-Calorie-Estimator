7514acf09ed3323efd5e979cf8f30aee6626e12b
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-13 23:14:13 +0100

    updated studyPlan page styles and content

--- src/pages/StudyPlan.tsx
+++ src/pages/StudyPlan.tsx
@@ -2,10 +2,12 @@ import Header from "../components/Header";
 
 const StudyPlan = () => {
   return (
-    <div className="flex h-[90vh] text-center justify-center items-center text-textcolor text-2xl">
+    <main className="relative w-full min-h-screen bg-bg flex flex-col self-center px-[2%] py-3 transition-all duration-500 ease-in-out md:px-[5%] lg:px-[10%]">
       <Header />
-      StudyPlan
-    </div>
+      <div className="text-center text-2xl flex justify-center items-center text-textcolor h-[70vh]">
+        Study Plan
+      </div>
+    </main>
   );
 };
 
