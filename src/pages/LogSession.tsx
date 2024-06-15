ae14939fd79d57f495605d6c4c08bc6a7317c495
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-15 09:07:31 +0100

    removing unused imports

--- src/pages/LogSession.tsx
+++ src/pages/LogSession.tsx
@@ -5,47 +5,21 @@ import {
 } from "../features/store/auth/authSlice";
 import { Navigate } from "react-router-dom";
 import Header from "../components/Header";
-import { FormEvent, useEffect, useRef, useState } from "react";
 import Loader from "../assets/loader.json";
 import ProfileCenter from "../components/profileForm/ProfileCenter";
 import { useGetSubjectsQuery } from "../features/Apislices/ProfileApiSlice";
 // If you are using date-fns v2.x, please import `AdapterDateFns`
-import { toast } from "sonner";
 import Lottie from "lottie-react";
-import { useLogSessionMutation } from "../features/Apislices/StudySessionApiSlice";
 
 const LogSession = () => {
   const currentUser = useSelector(getCurrentUser);
   const userLoggedIn = useSelector(getUserLoggedIn);
-  const sliderRef = useRef<HTMLInputElement>(null);
-  const [range, setRange] = useState(30);
   // const [date, setDate] = useState<Date>(new Date());
-  const [errorMessage, setErrorMessage] = useState("");
   const { data: subjects, isLoading: gettingSubjects } = useGetSubjectsQuery({
     email: currentUser?.email,
   });
-  const [subject, setSubject] = useState("");
-  useEffect(() => {
-    if (subjects?.length) {
-      setSubject(subjects[0].value);
-    }
-  }, [subjects]);
 
-  const [logSession, { isLoading: LoggingSession }] = useLogSessionMutation();
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const response = await logSession({
-        email: currentUser?.email,
-        subject,
-        duration: range,
-      }).unwrap();
-      toast.success(response?.message);
-    } catch (err: any) {
-      setErrorMessage(err.response);
-    }
-  };
   return (
     <>
       {!userLoggedIn && <Navigate to={"/login"} replace={true} />}
@@ -54,7 +28,6 @@ const LogSession = () => {
       )}
       <main className="relative w-full min-h-screen bg-bg flex flex-col self-center place-items-center px-[2%] py-3 transition-all duration-500 ease-in-out md:px-[5%] lg:px-[10%]">
         <Header />
-        <ChatBot />
 
         {gettingSubjects ? (
           <main className="w-full h-screen flex self-center place-content-center place-items-center">
