6d14af6d666aedd4dc86cfda5a0064c07625694c
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-15 09:09:23 +0100

    added subject select to logsession form

--- src/pages/LogSession.tsx
+++ src/pages/LogSession.tsx
@@ -5,21 +5,46 @@ import {
 } from "../features/store/auth/authSlice";
 import { Navigate } from "react-router-dom";
 import Header from "../components/Header";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import Loader from "../assets/loader.json";
 import ProfileCenter from "../components/profileForm/ProfileCenter";
 import { useGetSubjectsQuery } from "../features/Apislices/ProfileApiSlice";
 // If you are using date-fns v2.x, please import `AdapterDateFns`
+import { toast } from "sonner";
 import Lottie from "lottie-react";
+import { useLogSessionMutation } from "../features/Apislices/StudySessionApiSlice";
 
 const LogSession = () => {
   const currentUser = useSelector(getCurrentUser);
   const userLoggedIn = useSelector(getUserLoggedIn);
+  const sliderRef = useRef<HTMLInputElement>(null);
+  const [range, setRange] = useState(30);
   // const [date, setDate] = useState<Date>(new Date());
+  const [errorMessage, setErrorMessage] = useState("");
   const { data: subjects, isLoading: gettingSubjects } = useGetSubjectsQuery({
     email: currentUser?.email,
   });
+  const [subject, setSubject] = useState("");
+  useEffect(() => {
+    if (subjects?.length) {
+      setSubject(subjects[0].value);
+    }
+  }, [subjects]);
 
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await logSession({
+        email: currentUser?.email,
+        subject,
+        duration: range,
+      }).unwrap();
+      toast.success(response?.message);
+    } catch (err: any) {
+      setErrorMessage(err.response);
+    }
+  };
   return (
     <>
       {!userLoggedIn && <Navigate to={"/login"} replace={true} />}
@@ -28,7 +53,6 @@ const LogSession = () => {
       )}
       <main className="relative w-full min-h-screen bg-bg flex flex-col self-center place-items-center px-[2%] py-3 transition-all duration-500 ease-in-out md:px-[5%] lg:px-[10%]">
         <Header />
-
         {gettingSubjects ? (
           <main className="w-full h-screen flex self-center place-content-center place-items-center">
             <Lottie
@@ -47,6 +71,26 @@ const LogSession = () => {
                   </h3>
                 </div>
               </div>
+              <form className="space-y-5" onSubmit={handleSubmit}>
+                <div className="">
+                  <label htmlFor="subjects" className="input-label">
+                    subject
+                  </label>
+                  <select
+                    name="subjects"
+                    id="subjects"
+                    value={subject}
+                    className="basic-input"
+                    onChange={(e) => setSubject(e.target.value)}
+                  >
+                    {subjects?.map(
+                      (subject: { value: string; label: string }) => (
+                        <option value={subject.value}>{subject.label}</option>
+                      )
+                    )}
+                  </select>
+                </div>
+              </form>
             </ProfileCenter>
           </div>
         )}
