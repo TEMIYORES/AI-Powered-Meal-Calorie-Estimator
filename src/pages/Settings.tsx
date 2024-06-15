e0c994fc4d7b42282ac93e7a13ba3c6dd17faf8f
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-15 16:54:32 +0100

    setting up settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,13 +1,32 @@
+import { useSelector } from "react-redux";
+import {
+  getCurrentUser,
+  getUserLoggedIn,
+} from "../features/store/auth/authSlice";
+import { Navigate } from "react-router-dom";
 import Header from "../components/Header";
+import ChatBot from "../components/ChatBot";
+
+// If you are using date-fns v2.x, please import `AdapterDateFns`
 
 const Settings = () => {
+  const currentUser = useSelector(getCurrentUser);
+  const userLoggedIn = useSelector(getUserLoggedIn);
+  // const [date, setDate] = useState<Date>(new Date());
+
   return (
-    <main className="relative w-full min-h-screen bg-bg flex flex-col self-center px-[2%] py-3 transition-all duration-500 ease-in-out md:px-[5%] lg:px-[10%]">
-      <Header />
-      <div className="text-center text-2xl flex justify-center items-center text-textcolor h-[70vh]">
-        Settings
-      </div>
-    </main>
+    <>
+      {!userLoggedIn && <Navigate to={"/login"} replace={true} />}
+      {!currentUser?.profileSetup && (
+        <Navigate to={"/profile"} replace={true} />
+      )}
+      <main className="relative w-full min-h-screen bg-bg flex flex-col self-center place-items-center px-[2%] py-3 transition-all duration-500 ease-in-out md:px-[5%] lg:px-[10%]">
+        <Header />
+        <ChatBot />
+
+        {/* <StudyPlan studyPlan={studyPlan} /> */}
+      </main>
+    </>
   );
 };
 
