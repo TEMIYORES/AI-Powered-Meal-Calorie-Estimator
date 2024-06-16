4732fba45709e1ae5fdc00b6d4d47b9158b78509
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-16 20:44:38 +0100

    added education level select to profile settings form

--- src/components/SettingsProfile.tsx
+++ src/components/SettingsProfile.tsx
@@ -17,12 +17,16 @@ const SettingsProfile = () => {
     email: currentUser?.email,
   });
   const [age, setAge] = useState(profile?.age);
-
+  const [educationLevel, setEducationLevel] = useState<
+    "HighSchool" | "Undergraduate" | "Graduate"
+  >(profile?.educationLevel);
+  
   useEffect(() => {
     refetch();
   }, [profile]);
   useEffect(() => {
     setAge(profile?.age);
+    setEducationLevel(profile?.educationLevel);
   }, [profile]);
   return gettingProfile ? (
     <div className="flex-grow w-full flex items-center justify-center">
@@ -46,6 +50,30 @@ const SettingsProfile = () => {
             }}
             className="basic-input"
           />
+        </div>
+        <div className="w-full">
+          <label htmlFor="educationLevel" className="input-label">
+            What is your current education level?
+          </label>
+          <select
+            name="educationLevel"
+            id="educationLevel"
+            value={educationLevel}
+            onChange={(e) => {
+              if (
+                e.target.value === "HighSchool" ||
+                e.target.value === "Undergraduate" ||
+                e.target.value === "Graduate"
+              ) {
+                setEducationLevel(e.target.value);
+              }
+            }}
+            className="basic-input"
+          >
+            <option value="HighSchool">High School</option>
+            <option value="Undergraduate">Undergraduate</option>
+            <option value="Graduate">Graduate</option>
+          </select>
         </div>
       </div>
     </div>
