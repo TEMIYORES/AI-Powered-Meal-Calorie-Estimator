5de98085f47d84b448accf2dd7d8cf3d8e2814e5
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-06-17 21:53:51 +0100

    added subject select option to settings profile form

--- src/components/SettingsProfile.tsx
+++ src/components/SettingsProfile.tsx
@@ -1,11 +1,14 @@
 import "@wojtekmaj/react-timerange-picker/dist/TimeRangePicker.css";
 import Lottie from "lottie-react";
 import { useEffect, useState } from "react";
+import AllSubjects, { option } from "../assets/subjects";
+import Select from "react-select";
 import { useSelector } from "react-redux";
 import Loader from "../assets/loader.json";
 import { useGetProfileQuery } from "../features/Apislices/ProfileApiSlice";
 import { getCurrentUser } from "../features/store/auth/authSlice";
 import "../theme/Timerangepicker.css";
+import { customStyles } from "../theme/Styles";
 
 const SettingsProfile = () => {
   const currentUser = useSelector(getCurrentUser);
@@ -20,12 +23,15 @@ const SettingsProfile = () => {
   const [educationLevel, setEducationLevel] = useState<
     "HighSchool" | "Undergraduate" | "Graduate"
   >(profile?.educationLevel);
-  
+  const [subjects, setSubjects] = useState<readonly option[]>(
+    profile?.subjects
+  );
   useEffect(() => {
     refetch();
   }, [profile]);
   useEffect(() => {
     setAge(profile?.age);
+    setSubjects(profile?.subjects);
     setEducationLevel(profile?.educationLevel);
   }, [profile]);
   return gettingProfile ? (
@@ -74,6 +80,19 @@ const SettingsProfile = () => {
             <option value="Undergraduate">Undergraduate</option>
             <option value="Graduate">Graduate</option>
           </select>
+        </div>
+        <div className="w-full">
+          <label htmlFor="educationLevel" className="input-label">
+            Which subjects are you currently studying?
+          </label>
+          <Select
+            options={AllSubjects[educationLevel] as option[]}
+            isMulti
+            styles={customStyles}
+            value={subjects}
+            onChange={(values) => setSubjects(values)}
+            className="w-full"
+          />
         </div>
       </div>
     </div>
